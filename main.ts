532c4c907bb289432d95d4064c29310427fdff59
Carlos "Café Café" García <58571285+Carlos-CafeCafe@users.noreply.github.com>
2023-07-13 22:24:19 -0300

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,22 @@
-input.onButtonPressed(Button.A, function () {
-	
-})
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "tanqueLleno") {
         medirConductividad()
         radio.sendValue("conductividad", Conductividad)
     }
 })
-input.onButtonPressed(Button.B, function () {
-	
-})
 function medirConductividad () {
     Conductividad = pins.analogReadPin(AnalogPin.P0)
-    basic.showNumber(Conductividad)
+    proporcionalidad = pins.map(
+    Conductividad,
+    0,
+    1023,
+    0,
+    100
+    )
+    basic.showNumber(Math.round(proporcionalidad))
 }
+let proporcionalidad = 0
 let Conductividad = 0
+let Humedad = 0
 radio.setGroup(1)
 Conductividad = 0
-let Humedad = 0
